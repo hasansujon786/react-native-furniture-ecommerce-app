83913032acf3eef31ebf4172baa546269f7add6b
hasansujon786 <hasansujon786@gmail.com>
2021-08-26 22:47:19 +0600

    remove screen photos

--- components/AnimatedHeader.js
+++ components/AnimatedHeader.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { HStack, IconButton, Stack, Text } from 'native-base';
 import { StyleSheet, Platform } from 'react-native'
 import Animated, { useAnimatedStyle, interpolate, interpolateColor, Extrapolate } from 'react-native-reanimated'
+import { useNavigation } from '@react-navigation/native'
 
 import FilterBar from './FilterBar'
 import Icon from './Icon'
@@ -11,6 +12,8 @@ export const HEADER_MIN_HEIGHT = 80
 export const HEADER_MAX_HEIGHT = HEADER_MIN_HEIGHT + 70
 
 const AnimatedHeader = ({ scrollY }) => {
+  const navigation = useNavigation()
+
   const rHeaderStyles = useAnimatedStyle(() => {
     const height = interpolate(
       scrollY.value,
@@ -48,14 +51,18 @@ const AnimatedHeader = ({ scrollY }) => {
   return (
     <Animated.View style={[styles.animatedHeaderContainer, rHeaderStyles]}>
       <HStack style={{ height: HEADER_MIN_HEIGHT }} px={2} pt={8} justifyContent='space-between' alignItems='flex-start'>
-        <IconButton rounded='pill' variant='ghost' colorScheme='light'
+        <IconButton
+          onPress={() => navigation.navigate('Search')}
+          rounded='pill' variant='ghost' colorScheme='light'
           icon={<Icon name='search-outline' size='sm' />}
         />
         <Stack justifyContent='center' alignItems='center'>
           <Text mr={1} fontSize='xs' color='muted.400' fontWeight='bold'>MAKE HOME</Text>
           <Text fontWeight='bold'>BEAUTIFUL</Text>
         </Stack>
-        <IconButton rounded='pill' variant='ghost' colorScheme='light'
+        <IconButton
+          rounded='pill' variant='ghost' colorScheme='light'
+          onPress={() => navigation.navigate('Cart')}
           icon={<Icon name='cart-outline' size='sm' />}
         />
       </HStack>
